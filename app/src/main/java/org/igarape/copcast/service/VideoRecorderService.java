1c19fdadfe1dbbda6a2390f7f8a333fe5a319681
Leandro Martelli <martelli@epix.com.br>
2015-10-15 18:06:08 -0300

    Cleaned up VideoRecorder service release procedures. Use of locks to avoid preparing and releasing the mediaRecorder object at the same time (race condition).

--- app/src/main/java/org/igarape/copcast/service/VideoRecorderService.java
+++ app/src/main/java/org/igarape/copcast/service/VideoRecorderService.java
@@ -5,7 +5,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.media.CamcorderProfile;
@@ -14,14 +13,12 @@ import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.WindowManager;
-import android.widget.Toast;
 
 import org.igarape.copcast.R;
 import org.igarape.copcast.utils.FileUtils;
@@ -29,12 +26,10 @@ import org.igarape.copcast.utils.Globals;
 import org.igarape.copcast.views.MainActivity;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.locks.ReentrantLock;
 
-/**
- * Created by fcavalcanti on 19/11/2014.
- */
+
 public class VideoRecorderService extends Service implements SurfaceHolder.Callback {
 
     private static final String TAG = VideoRecorderService.class.getName();
@@ -45,9 +40,12 @@ public class VideoRecorderService extends Service implements SurfaceHolder.Callb
     private MediaRecorder mediaRecorder = null;
     private boolean isRecording;
     protected SurfaceHolder surfaceHolder;
-    public static final int MAX_DURATION_MS = 300000;
+    public static final int MAX_DURATION_MS = 2000;
     public static final long MAX_SIZE_BYTES = 7500000;
     private int mId = 1;
+    private ReentrantLock lock = new ReentrantLock();
+    private boolean serviceExiting = false;
+    public static boolean serviceRunning = false;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -55,6 +53,8 @@ public class VideoRecorderService extends Service implements SurfaceHolder.Callb
             stopSelf();
             return START_STICKY;
         }
+
+        serviceRunning = true;
 
         Intent resultIntent = new Intent(this, MainActivity.class);
         Context context = getApplicationContext();
@@ -98,74 +98,78 @@ public class VideoRecorderService extends Service implements SurfaceHolder.Callb
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
         this.surfaceHolder = surfaceHolder;
-        new MediaPrepareTask().execute(null, null, null);
-    }
-
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
+        new MediaPrepareTask().execute();
     }
 
     private boolean prepareMediaEncoder() {
 
-        camera = getCameraInstance();
-        //set camera to continually auto-focus
-        Camera.Parameters params = camera.getParameters();
-        //*EDIT*//params.setFocusMode("continuous-picture");
-        //It is better to use defined constraints as opposed to String, thanks to AbdelHady
-        params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
-        camera.setParameters(params);
-        if (camera == null){
-            Log.e(TAG, "Camera returned null");
-            stopSelf();
+        if (serviceExiting)
             return false;
-        }
-        mediaRecorder = new MediaRecorder();
-        camera.unlock();
 
-        mediaRecorder.setPreviewDisplay(this.surfaceHolder.getSurface());
-        mediaRecorder.setCamera(camera);
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
-        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-        mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_LOW));
-
-        mediaRecorder.setOutputFile(
-                FileUtils.getPath(Globals.getUserLogin(getBaseContext())) +
-                        android.text.format.DateFormat.format("yyyy-MM-dd_kk-mm-ss", new Date().getTime()) +
-                        ".mp4");
-
-        mediaRecorder.setOrientationHint(getScreenOrientation(Globals.getRotation()));
-        mediaRecorder.setMaxDuration(MAX_DURATION_MS);
-        mediaRecorder.setMaxFileSize(MAX_SIZE_BYTES);
-        mediaRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
-            @Override
-            public void onInfo(MediaRecorder mediaRecorder, int what, int extra) {
-                if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED ||
-                        what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED) {
-                    releaseMediaRecorder();
-                    new MediaPrepareTask().execute(null, null, null);
-                }
-            }
-        });
-
+        lock.lock();
+        Log.d(TAG, "> prepare locked");
         try {
-            mediaRecorder.prepare();
-        } catch (IOException e) {
-            Log.e(TAG,"ioException on prepareMediaEncoder", e);
-            return false;
+            try {
+                camera = Camera.open(); // attempt to get a Camera instance
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to open camera.");
+                return false;
+            }
+
+            Camera.Parameters params = camera.getParameters();
+            params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+            camera.setParameters(params);
+            camera.unlock();
+
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setPreviewDisplay(this.surfaceHolder.getSurface());
+            mediaRecorder.setCamera(camera);
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+            mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_LOW));
+
+            mediaRecorder.setOutputFile(
+                    FileUtils.getPath(Globals.getUserLogin(getBaseContext())) +
+                            android.text.format.DateFormat.format("yyyy-MM-dd_kk-mm-ss", new Date().getTime()) +
+                            ".mp4");
+
+            mediaRecorder.setOrientationHint(getScreenOrientation(Globals.getRotation()));
+            mediaRecorder.setMaxDuration(MAX_DURATION_MS);
+            mediaRecorder.setMaxFileSize(MAX_SIZE_BYTES);
+            mediaRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
+                @Override
+                public void onInfo(MediaRecorder mediaRecorder, int what, int extra) {
+                    if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED ||
+                            what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED) {
+                        releaseMediaRecorder();
+                        new MediaPrepareTask().execute();
+                    }
+                }
+            });
+
+            try {
+                mediaRecorder.prepare();
+            } catch (IOException e) {
+                Log.e(TAG, "ioException on prepareMediaEncoder");
+                return false;
+            }
+
+            mediaRecorder.start();
+
+            return true;
+
+        } finally {
+            lock.unlock();
+            Log.d(TAG, "< prepare unlocked");
         }
-        return true;
     }
 
     @Override
     public void onDestroy() {
+
+        serviceExiting = true;
         releaseMediaRecorder();
+
         if(null != windowManager && null != surfaceView){
             windowManager.removeView(surfaceView);
             Log.d(TAG, "onDestroy with windowManager=["+windowManager+" and surfaceView=["+surfaceView+"]");
@@ -192,6 +196,7 @@ public class VideoRecorderService extends Service implements SurfaceHolder.Callb
             intentAux.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startService(intentAux);
         }
+        serviceRunning = false;
     }
 
     @Override
@@ -200,57 +205,44 @@ public class VideoRecorderService extends Service implements SurfaceHolder.Callb
     }
 
     private void releaseMediaRecorder() {
+
+        lock.lock();
+        Log.d(TAG, "> release locked");
         try {
             if (mediaRecorder != null) {
-                mediaRecorder.stop();
+//                mediaRecorder.stop();
                 mediaRecorder.reset();
                 mediaRecorder.release();
                 mediaRecorder = null;
             }
+
+            if (camera != null) {
+//                try {
+//                    camera.stopPreview();
+//                } catch (Exception e){
+//                    Log.w(TAG, "releasing camera 1", e);
+//                }
+//                try {
+//                    camera.lock();
+                camera.release();
+//                } catch (Exception e){
+//                    Log.w(TAG, "releasing camera 2", e);
+//                }
+            }
+
         } catch (IllegalStateException i) {
             Log.e(TAG,"IllegalStateException on prepareMediaEncoder", i);
+        } finally {
+            lock.unlock();
+            Log.d(TAG, "< release unlocked");
         }
-
-        if (camera != null) {
-            try {
-                camera.stopPreview();
-            } catch (Exception e){
-                Log.e(TAG, "releasing camera", e);
-                //
-            }
-            try {
-                camera.lock();
-                camera.release();
-            } catch (Exception e){
-                Log.e(TAG, "releasing camera", e);
-                //
-            }
-        }
-
     }
 
     class MediaPrepareTask extends AsyncTask<Void, Void, Boolean> {
 
         @Override
-        protected Boolean doInBackground(Void... voids) {
-            if (prepareMediaEncoder()) {
-                mediaRecorder.start();
-                isRecording = true;
-            } else {
-                releaseMediaRecorder();
-                isRecording = false;
-                return false;
-            }
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean result) {
-            if (!result) {
-                //MainActivity.this.finish();
-            }
-            // inform the user that recording has started
-            //setCaptureButtonText("Stop");
+        protected Boolean doInBackground(Void... flags) {
+            return prepareMediaEncoder();
         }
     }
 
