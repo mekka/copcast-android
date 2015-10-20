1e29681ad2d978accde0e8bdd823f91e69a0b66b
alex <alexsalgado1@gmail.com>
2015-10-20 14:40:08 -0200

    Reporting Form creation (for Cape Town Traffic Police) #95

---
+++ app/src/main/java/org/igarape/copcast/views/FormIncidentReportActivity.java
@@ -0,0 +1,15 @@
+package org.igarape.copcast.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.igarape.copcast.R;
+
+public class FormIncidentReportActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_incident_report);
+    }
+}
