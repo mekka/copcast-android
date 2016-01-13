dfd7441bfe9bb3c9c96430c0de5dcc9905e78e64
brunosiqueira <bsiqueira@gmail.com>
2016-01-13 16:27:36 +0000

    updating webrtc client version

--- app/src/main/java/org/igarape/copcast/service/StreamService.java
+++ app/src/main/java/org/igarape/copcast/service/StreamService.java
@@ -13,6 +13,7 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.view.Gravity;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.WindowManager;
 
 import org.igarape.copcast.R;
@@ -35,14 +36,8 @@ public class StreamService extends Service implements SurfaceHolder.Callback, We
     private static final String VIDEO_CODEC_VP9 = "VP9";
     private static final String AUDIO_CODEC_OPUS = "opus";
     private int mId = 5;
-    private VideoRenderer.Callbacks localRender;
-    private static final int LOCAL_X_CONNECTING = 0;
-    private static final int LOCAL_Y_CONNECTING = 0;
-    private static final int LOCAL_WIDTH_CONNECTING = 100;
-    private static final int LOCAL_HEIGHT_CONNECTING = 100;
-    private RendererCommon.ScalingType scalingType = RendererCommon.ScalingType.SCALE_ASPECT_FILL;
     private WindowManager mWindowManager;
-    private GLSurfaceView mSurfaceView;
+    private SurfaceView mSurfaceView;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -82,7 +77,7 @@ public class StreamService extends Service implements SurfaceHolder.Callback, We
 
         // Create new SurfaceView, set its size to 1x1, move it to the top left corner and set this service as a callback
         mWindowManager = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
-        mSurfaceView = new GLSurfaceView(this);
+        mSurfaceView = new SurfaceView(this);
         WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
                 1, 1,
                 WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
@@ -91,19 +86,8 @@ public class StreamService extends Service implements SurfaceHolder.Callback, We
         );
         layoutParams.gravity = Gravity.LEFT | Gravity.TOP;
         mWindowManager.addView(mSurfaceView, layoutParams);
-        VideoRendererGui.setView(mSurfaceView, new Runnable() {
-            @Override
-            public void run() {
-                init();
-            }
-
-
-        });
         mSurfaceView.getHolder().addCallback(this);
 
-        localRender = VideoRendererGui.create(
-                LOCAL_X_CONNECTING, LOCAL_Y_CONNECTING,
-                LOCAL_WIDTH_CONNECTING, LOCAL_HEIGHT_CONNECTING, scalingType, true);
         return START_STICKY;
     }
 
@@ -112,18 +96,17 @@ public class StreamService extends Service implements SurfaceHolder.Callback, We
 
         CamcorderProfile profile = CamcorderProfile.get(CamcorderProfile.QUALITY_LOW);
 
-        //windowManager.getDefaultDisplay().getSize(displaySize);
         PeerConnectionParameters params = new PeerConnectionParameters(
                 true, false, profile.videoFrameWidth, profile.videoFrameHeight, 15, 1, VIDEO_CODEC_VP9, true, 1, AUDIO_CODEC_OPUS, true);
 
-        client = new WebRtcClient(this, Globals.getServerUrl(getApplicationContext()), params, VideoRendererGui.getEGLContext(), Globals.getAccessTokenStraight(getApplicationContext()));
+        client = new WebRtcClient(this, Globals.getServerUrl(getApplicationContext()), params, Globals.getAccessTokenStraight(getApplicationContext()));
 
 
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
+        init();
     }
 
     @Override
@@ -146,9 +129,6 @@ public class StreamService extends Service implements SurfaceHolder.Callback, We
     public void onDestroy() {
         if(client != null) {
             client.onDestroy();
-            if (localRender != null) {
-                VideoRendererGui.remove(localRender);
-            }
             if (mSurfaceView != null) {
                 mWindowManager.removeView(mSurfaceView);
             }
@@ -175,10 +155,5 @@ public class StreamService extends Service implements SurfaceHolder.Callback, We
 
     @Override
     public void onLocalStream(MediaStream localStream) {
-        localStream.videoTracks.get(0).addRenderer(new VideoRenderer(localRender));
-        VideoRendererGui.update(localRender,
-                LOCAL_X_CONNECTING, LOCAL_Y_CONNECTING,
-                LOCAL_WIDTH_CONNECTING, LOCAL_HEIGHT_CONNECTING,
-                scalingType, false);
     }
 }
