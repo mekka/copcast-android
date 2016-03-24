2493facef53428ffc2d4a8ef90021f362eb14a15
Leandro Martelli <martelli@epix.com.br>
2016-03-24 13:31:08 -0300

    removed unused CountDownStreamTimer inner class.

--- app/src/main/java/org/igarape/copcast/views/MainActivity.java
+++ app/src/main/java/org/igarape/copcast/views/MainActivity.java
@@ -749,30 +749,6 @@ public class MainActivity extends Activity {
         }
     }
 
-    private class CountDownStreamTimer extends CountDownTimer {
-
-        /**
-         * @param millisInFuture    The number of millis in the future from the call
-         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
-         *                          is called.
-         * @param countDownInterval The interval along the way to receive
-         *                          {@link #onTick(long)} callbacks.
-         */
-        public CountDownStreamTimer(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
-        }
-
-        @Override
-        public void onTick(long millisUntilFinished) {
-
-        }
-
-        @Override
-        public void onFinish() {
-            mStreamSwitch.setChecked(false);
-        }
-    }
-
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
