ceea05db065a06ad762288f2422fd396b08efd25
Leandro Martelli <martelli@epix.com.br>
2016-05-03 18:10:57 -0300

    Removed stalled code from SplashScreen and moved some initialization to CopcastApplication.

--- app/src/main/java/org/igarape/copcast/CopCastApplication.java
+++ app/src/main/java/org/igarape/copcast/CopCastApplication.java
@@ -2,9 +2,15 @@ package org.igarape.copcast;
 
 import android.app.Application;
 import android.content.res.Configuration;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
 
 import org.igarape.copcast.utils.FileUtils;
 import org.igarape.copcast.utils.Globals;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by fcavalcanti on 27/10/2014.
@@ -22,6 +28,20 @@ public class CopCastApplication extends Application {
         Globals.sessionInit();
         Globals.initStateManager(this);
         FileUtils.init(getApplicationContext());
+        Globals.setAccessToken(this, null);
+        Globals.setDirectorySize(getApplicationContext(), FileUtils.getDirectorySize());
+
+        String fileName = "logcat_"+System.currentTimeMillis()+".txt";
+        File outputFile = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES),fileName);
+        try {
+            @SuppressWarnings("unused")
+            Process process = Runtime.getRuntime().exec("logcat -f "+outputFile.getAbsolutePath());
+            Log.e("DEBUG", outputFile.getAbsolutePath());
+        } catch (IOException e) {
+            Toast.makeText(this, "Logcat error", Toast.LENGTH_LONG).show();
+        }
+
     }
 
 
