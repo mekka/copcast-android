2ff5b30648e7e51588cdebd2c87c2891fdbfe51d
Daniel Borkan <dborkan@dborkan-macbookpro2.roam.corp.google.com>
2016-07-29 13:46:53 -0400

    Fix bad merge #2

--- app/src/main/java/org/igarape/copcast/service/LocationService.java
+++ app/src/main/java/org/igarape/copcast/service/LocationService.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
